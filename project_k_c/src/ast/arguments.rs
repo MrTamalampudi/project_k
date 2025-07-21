9a4a37f8e0049436d1a79c1df9f3ddbf5b16e254
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-07-21 16:11:02 +0530

    add new arg keys

--- project_k_c/src/ast/arguments.rs
+++ project_k_c/src/ast/arguments.rs
@@ -2,9 +2,12 @@ use crate::parser::locator::LocatorStrategy;
 
 pub const LOCATOR_ARGKEY: &'static str = "locator";
 pub const URL_ARGKEY: &'static str = "url";
+pub const ATTRIBUTE_ARGKEY: &'static str = "attribute";
 
 #[derive(Debug, Clone)]
 pub enum Args {
     String(String),
     Locator(LocatorStrategy),
+    Ident(String),
+    None,
 }
