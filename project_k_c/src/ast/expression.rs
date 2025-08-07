a805502f061fe65eeb3c1af101b0b9a63dc1003e
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-08-07 11:38:54 +0530

    redesign expression enum structure

--- project_k_c/src/ast/expression.rs
+++ project_k_c/src/ast/expression.rs
@@ -2,40 +2,43 @@
 
 #[derive(Debug, Clone)]
 pub enum Expression {
-    BinaryExpression(BinaryExpression),
+    Literal(LiteralExpression),
+    OperatorExpression(Box<OperatorExpression>),
+    GroupedExpression(Box<Expression>),
 }
 
 #[derive(Debug, Clone)]
-pub struct BinaryExpression {
-    left: Box<Expression>,
-    right: Box<Expression>,
-    operator: BinOpKind,
+pub enum LiteralExpression {
+    Number(isize),
+    Ident(String),
+    String(String),
+    Boolean(bool),
 }
 
 #[derive(Debug, Clone)]
-pub enum BinOpKind {
-    /// The `+` operator (addition)
-    Add,
-    /// The `-` operator (subtraction)
-    Sub,
-    /// The `*` operator (multiplication)
-    Mul,
-    /// The `/` operator (division)
-    Div,
-    /// The `%` operator (modulus)
-    Rem,
-    /// The `&&` operator (logical and)
-    And,
-    /// The `||` operator (logical or)
-    Eq,
-    /// The `<` operator (less than)
-    Lt,
-    /// The `<=` operator (less than or equal to)
-    Le,
-    /// The `!=` operator (not equal to)
-    Ne,
-    /// The `>=` operator (greater than or equal to)
-    Ge,
-    /// The `>` operator (greater than)
-    Gt,
+pub enum OperatorExpression {
+    Negation(Expression),
+    ArthimaticExpression(ArthimaticExpression),
+    ComparisionExpression(ComparisionExpression),
+}
+
+//Here LiteralExpression is used for return type
+#[derive(Debug, Clone)]
+pub enum ArthimaticExpression {
+    Plus(Expression, Expression, LiteralExpression),
+    Minus(Expression, Expression, LiteralExpression),
+    SPMinus(Expression, Expression, LiteralExpression),
+    Multiply(Expression, Expression, LiteralExpression),
+    Division(Expression, Expression, LiteralExpression),
+    Modulus(Expression, Expression, LiteralExpression),
+}
+
+#[derive(Debug, Clone)]
+pub enum ComparisionExpression {
+    Equality(Expression, Expression),
+    NotEqual(Expression, Expression),
+    GreaterThan(Expression, Expression),
+    LesserThan(Expression, Expression),
+    GreaterThanEqual(Expression, Expression),
+    LesserThanEqual(Expression, Expression),
 }
