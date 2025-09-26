3bc1371146428e1c924b454c8af2e40c1213e5af
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-09-26 13:12:01 +0530

    Add expression Arg key

--- project_k_c/src/ast/arguments.rs
+++ project_k_c/src/ast/arguments.rs
@@ -1,12 +1,14 @@
-use crate::parser::locator::LocatorStrategy;
+use crate::{ast::expression::Expr, parser::locator::LocatorStrategy};
 
 pub const LOCATOR_ARGKEY: &'static str = "locator";
 pub const URL_ARGKEY: &'static str = "url";
 pub const ATTRIBUTE_ARGKEY: &'static str = "attribute";
 pub const SECS_ARGKEY: &'static str = "seconds";
+pub const EXPR_ARGKEY: &'static str = "expression";
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Args {
+    Expr(Expr),
     String(String),
     Locator(LocatorStrategy),
     Ident(String),
