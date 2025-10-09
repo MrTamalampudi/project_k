14c5d88bed9500ef447c66b0b9a5f73229f1c7c8
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-10-09 15:43:20 +0530

    remove some git scrap

--- project_k_c/src/parser/actions/binary_expr.rs
+++ project_k_c/src/parser/actions/binary_expr.rs
@@ -1,21 +1,14 @@
 use manodae::error::ParseError;
 
-<<<<<<< HEAD
 use crate::parser::errors::MISMATCHED_TYPES;
 use crate::parser::errorss::ActionError;
 use crate::parser::translator_stack::TLVec;
-=======
-use crate::location::SpanTrait;
-use crate::parser::errors::MISMATCHED_TYPES;
-use crate::parser::errorss::ActionError;
->>>>>>> origin/master
 use crate::{
     ast::{
         expression::{BinOpKind, ExpKind, Expr},
         testcase::TestCase,
     },
     class::BinaryExpressionAction,
-<<<<<<< HEAD
     parser::translator_stack::TranslatorStack,
     token::Token,
 };
@@ -68,15 +61,6 @@ impl BinaryExpression {
 }
 
 impl BinaryExpressionAction for BinaryExpression {
-=======
-    parser::{errors::EXPECT_EXPR, translator_stack::TranslatorStack},
-    token::Token,
-};
-
-pub struct BE;
-
-impl BinaryExpressionAction for BE {
->>>>>>> origin/master
     //expr + expr
     fn ADD(
         _testcase: &mut TestCase,
@@ -84,41 +68,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Add);
-=======
-        let plus_token = _token_stack.pop().unwrap(); //pop 'plus' token
-
-        let expr2_tl = _tl_stack.pop().unwrap();
-        let expr2 = if let TranslatorStack::Expression(expr) = expr2_tl {
-            expr
-        } else {
-            _errors.push_error(&plus_token, &expr2_tl.get_span(), EXPECT_EXPR.to_string());
-            return;
-        };
-
-        let expr1_tl = _tl_stack.pop().unwrap();
-        let expr1 = if let TranslatorStack::Expression(expr) = expr1_tl {
-            expr
-        } else {
-            _errors.push_error(&plus_token, &expr1_tl.get_span(), EXPECT_EXPR.to_string());
-            return;
-        };
-
-        let span = expr1.span.to(&expr2.span);
-        if expr1.primitive != expr2.primitive {
-            _errors.push_error(&plus_token, &span, MISMATCHED_TYPES.to_string());
-            return;
-        }
-        let primitive = expr1.primitive.clone();
-        let kind = ExpKind::Binary(BinOpKind::Add, Box::new(expr1), Box::new(expr2));
-        let expr = Expr {
-            kind,
-            span,
-            primitive,
-        };
-        _tl_stack.push(TranslatorStack::Expression(expr));
->>>>>>> origin/master
     }
     fn SUB(
         _testcase: &mut TestCase,
@@ -126,7 +76,6 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Sub);
     }
     // expr expr
@@ -134,21 +83,15 @@ impl BinaryExpressionAction for BE {
     // so we're getting last token from the stack and adding again to the stack
     // just to meet the requirements
     // for any production there be should atleast one token for source file path
-=======
-    }
->>>>>>> origin/master
     fn SPL_SUB(
         _testcase: &mut TestCase,
         _token_stack: &mut Vec<Token>,
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         let token = _token_stack.last().unwrap().clone();
         _token_stack.push(token);
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Sub);
-=======
->>>>>>> origin/master
     }
     fn MUL(
         _testcase: &mut TestCase,
@@ -156,10 +99,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Mul);
-=======
->>>>>>> origin/master
     }
     fn DIV(
         _testcase: &mut TestCase,
@@ -167,10 +107,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Div);
-=======
->>>>>>> origin/master
     }
     fn REM(
         _testcase: &mut TestCase,
@@ -178,10 +115,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Rem);
-=======
->>>>>>> origin/master
     }
     fn AND(
         _testcase: &mut TestCase,
@@ -189,10 +123,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::And);
-=======
->>>>>>> origin/master
     }
     fn OR(
         _testcase: &mut TestCase,
@@ -200,10 +131,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Or);
-=======
->>>>>>> origin/master
     }
     fn EQ(
         _testcase: &mut TestCase,
@@ -211,10 +139,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Eq);
-=======
->>>>>>> origin/master
     }
     fn NE(
         _testcase: &mut TestCase,
@@ -222,10 +147,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Ne);
-=======
->>>>>>> origin/master
     }
     fn LT(
         _testcase: &mut TestCase,
@@ -233,10 +155,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Lt);
-=======
->>>>>>> origin/master
     }
     fn LE(
         _testcase: &mut TestCase,
@@ -244,10 +163,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Le);
-=======
->>>>>>> origin/master
     }
     fn GE(
         _testcase: &mut TestCase,
@@ -255,10 +171,7 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Ge);
-=======
->>>>>>> origin/master
     }
     fn GT(
         _testcase: &mut TestCase,
@@ -266,9 +179,6 @@ impl BinaryExpressionAction for BE {
         _tl_stack: &mut Vec<TranslatorStack>,
         _errors: &mut Vec<ParseError<Token>>,
     ) {
-<<<<<<< HEAD
         BinaryExpression::common(_testcase, _token_stack, _tl_stack, _errors, BinOpKind::Gt);
-=======
->>>>>>> origin/master
     }
 }
