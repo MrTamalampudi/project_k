7bc2f72acf88cca4fc3bb2f2e7de2bb394ed087f
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-10-22 13:36:28 +0530

    add pop_expr macro

--- project_k_c/src/parser/actions/mod.rs
+++ project_k_c/src/parser/actions/mod.rs
@@ -7,3 +7,16 @@ pub mod literal_expression;
 pub mod navigation;
 pub mod timeouts;
 pub mod unary_expr;
+
+#[macro_export]
+macro_rules! pop_expr {
+    ($pop_expr:expr,$error:ident,$token:ident) => {{
+        match $pop_expr {
+            Ok(expr) => expr,
+            Err((error, span)) => {
+                $error.push_error(&$token, &span, error);
+                return;
+            }
+        }
+    }};
+}
