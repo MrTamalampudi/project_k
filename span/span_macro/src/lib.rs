451d793463526c7a606c96c455f51576298b7ab9
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-11-20 19:07:05 +0530

    implement derive macro for span_trait

--- span/span_macro/src/lib.rs
+++ span/span_macro/src/lib.rs
@@ -1,37 +1,61 @@
-#[macro_use]
-extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DataEnum, DataStruct, DeriveInput, Ident, parse_macro_input, spanned::Spanned};
+
+const NO_SPAN_FIELD: &'static str = "No Span field on this struct";
+const SPAN: &'static str = "span";
+const UNION: &'static str = "Union Data type is not supported";
+
+type MacroResult = Result<proc_macro2::TokenStream, &'static str>;
 
 #[proc_macro_derive(Span)]
 pub fn span_trait_derive(input: TokenStream) -> TokenStream {
     let d_input = parse_macro_input!(input as DeriveInput);
     let name = d_input.ident;
-    match d_input.data {
-        syn::Data::Struct(struct_data) => span_trait_derive_struct(&struct_data),
-        syn::Data::Enum(enum_data) => span_trait_derive_enum(&enum_data),
-        syn::Data::Union(_) => span_trait_derive_union(),
+    let impl_ = match d_input.data {
+        syn::Data::Struct(struct_data) => span_trait_derive_struct(&name, &struct_data),
+        syn::Data::Enum(enum_data) => span_trait_derive_enum(&name, &enum_data),
+        syn::Data::Union(_) => Err(UNION),
+    };
+    if let Err(e) = impl_ {
+        return quote! {compile_error!(#e);}.into();
     }
+    impl_.unwrap().into()
 }
 
-fn span_trait_derive_struct(input: &DataStruct) -> TokenStream {
+fn span_trait_derive_struct(name: &Ident, input: &DataStruct) -> MacroResult {
     //check for span field
     let span = input
         .fields
         .iter()
-        .any(|field| field.ident == Some(Ident::new("span", field.ident.span())));
+        .any(|field| field.ident == Some(Ident::new(SPAN, field.ident.span())));
     if !span {
-        return quote! {
-            compile_error!("span field doesnt existdddddddddddddddd");
-        }
-        .into();
+        return Err(NO_SPAN_FIELD);
     };
-    TokenStream::new()
+    Ok(quote! {
+        impl SpanTrait for #name{
+            fn get_span(&self) -> Span {
+                self.span.clone()
+            }
+        }
+    })
 }
-fn span_trait_derive_enum(input: &DataEnum) -> TokenStream {
-    TokenStream::new()
-}
-fn span_trait_derive_union() -> TokenStream {
-    TokenStream::new()
+fn span_trait_derive_enum(name: &Ident, _enum: &DataEnum) -> MacroResult {
+    let mut match_arms = quote! {};
+    for variant in _enum.variants.iter() {
+        let v_ident = &variant.ident;
+        match_arms.extend(quote! {
+            #name::#v_ident(arg) => arg.get_span(),
+        });
+    }
+
+    Ok(quote! {
+        impl SpanTrait for #name{
+            fn get_span(&self) -> Span {
+                match self {
+                    #match_arms
+                }
+            }
+        }
+    })
 }
