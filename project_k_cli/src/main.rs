eab9c5b01a7a65dbf173efbeaf0d8fae7a1d1a8e
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2026-01-21 12:28:33 +0530

    update error styles

--- project_k_cli/src/main.rs
+++ project_k_cli/src/main.rs
@@ -1,7 +1,9 @@
 use engine::execute;
+use miette::GraphicalTheme;
 use miette::LabeledSpan;
 use miette::Result;
-use miette::SourceSpan;
+use miette::ThemeStyles;
+use owo_colors::style;
 use parser::parse;
 use parser::CompilationContext;
 use std::env;
@@ -12,16 +14,12 @@ fn main() -> Result<()> {
     log4rs::init_file("./log_config.yml", Default::default()).unwrap();
     env::set_var("RUST_BACKTRACE", "1");
     let argss: Vec<String> = env::args().collect();
+    miette_config();
     let source_path = match argss.get(1) {
         Some(path) => path,
         None => {
             let report = miette::miette!(
-                // Those fields are optional
                 severity = miette::Severity::Error,
-                code = "expected::rparen",
-                url = "https://example.com",
-                // Rest of the arguments are passed to `format!`
-                // to form diagnostic message
                 "Please provide a file path",
             );
             return Err(report);
@@ -31,15 +29,7 @@ fn main() -> Result<()> {
     let source = match fs::read_to_string(source_path) {
         Ok(string) => string,
         Err(error) => {
-            let report = miette::miette!(
-                // Those fields are optional
-                severity = miette::Severity::Error,
-                code = "expected::rparen",
-                url = "https://example.com",
-                // Rest of the arguments are passed to `format!`
-                // to form diagnostic message
-                "Please provide a file path",
-            );
+            let report = miette::miette!(severity = miette::Severity::Error, "{}", error);
             return Err(report);
         }
     };
@@ -57,19 +47,41 @@ fn main() -> Result<()> {
             .map(|e| LabeledSpan::at(e.span, e.message))
             .collect();
         let report = miette::miette!(
-            // Those fields are optional
             severity = miette::Severity::Error,
             code = "Parsing Error",
             help = "Please check documentation for clarity",
             labels = errors,
             url = "https://github.com",
-            // Rest of the arguments are passed to `format!`
-            // to form diagnostic message
             "Error while parsing the test case"
         )
         .with_source_code(source);
         return Err(report);
     }
-    println!("variables {:#?}", ctx.ast.testcase.variables);
     Ok(())
+}
+
+fn miette_config() {
+    miette::set_hook(Box::new(|_| {
+        let theme_style = ThemeStyles {
+            error: style().red(),
+            warning: style().yellow(),
+            advice: style().cyan(),
+            help: style().cyan(),
+            link: style().cyan().underline().bold(),
+            linum: style().dimmed(),
+            highlights: vec![style().yellow().bold()],
+        };
+        let mut graphical_theme = GraphicalTheme::unicode();
+        graphical_theme.styles = theme_style;
+        Box::new(
+            miette::MietteHandlerOpts::new()
+                .terminal_links(true)
+                .unicode(true)
+                .context_lines(2)
+                .break_words(true)
+                .graphical_theme(graphical_theme)
+                .build(),
+        )
+    }))
+    .unwrap();
 }
